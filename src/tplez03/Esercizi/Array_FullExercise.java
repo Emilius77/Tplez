69ce666a3b9dd3132ed6e4584b7bc9f001c458d4
Emilius <Emilius@192.168.1.135>
2016-11-16 18:20:27 +0100

    Esercitazione pre-Esame

--- src/tplez03/Esercizi/Array_FullExercise.java
+++ src/tplez03/Esercizi/Array_FullExercise.java
@@ -5,6 +5,7 @@
  */
 package tplez03.Esercizi;
 
+import com.sun.org.apache.bcel.internal.generic.ArithmeticInstruction;
 import javax.swing.JOptionPane;
 
 /**
@@ -20,13 +21,14 @@ public class Array_FullExercise {
         // TODO code application logic here
 
         int max_nomi = 0;                           // inizializzo la mia variabile che conterrà i nomi
+        
         String input = JOptionPane.showInputDialog("Quanti presenti ci sono?");  // chiedo l'input all'utente che mi indichi quanto deve essere grande nomi
         max_nomi = Integer.parseInt(input);         // assegno alla mia variabile "max_nomi" la grandezza indica dall'input utente
-
-        //int età = 0;
+        
+        
         //input = JOptionPane.showInputDialog("Quanti voto ha?");
         String nomi[] = new String[max_nomi];      // il mio Array nomi prenderà la grandezza assegnata alla variabile max_nomi
-        int voto [] = new int[max_nomi];
+        int voto[] = new int[max_nomi];
 
         for (int i = 0; i < nomi.length; i++) {
             // devo chiedere il nome e caricarlo nella scatola corretta
@@ -55,19 +57,26 @@ public class Array_FullExercise {
                     nome = nomi[i];
                 }
             }
+               
             int min = voto[0];
             String nome1 = nomi[0];
             for (int i = 0; i < max_nomi; i++) {
-                // guardo un numero per volta e guardo se è il maggiore
+                // guardo un numero per volta e guardo se è il minore
                 if (voto[i] < min) {
                     min = voto[i];
                     nome1 = nomi[i];
                 }
             }
+            // qui andrò a scrivere il codice per calcolare la media di tutti i voti inseriti
+            
+            int media = 0;
+            media = ((voto[0] + voto [1] + voto[2]) / max_nomi);
+            
             // stampo il report
             JOptionPane.showMessageDialog(null, report);        //visualizzo l'output richiesto
-            JOptionPane.showMessageDialog(null, "Il voto più alto è: " + " " + nome + " " + max);
-            JOptionPane.showMessageDialog(null, "Il voto più basso è: " + " " + nome1 + " " +  min);
+            JOptionPane.showMessageDialog(null, "Il voto più alto è di: " + " " + nome + " " + max);
+            JOptionPane.showMessageDialog(null, "Il voto più basso è di: " + " " + nome1 + " " +  min);
+            JOptionPane.showMessageDialog(null, "La media dei voti è: " + " " +  media);
             
         } 
     }
