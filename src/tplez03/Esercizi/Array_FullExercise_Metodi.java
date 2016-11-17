508171dccc267041f8452009880327b830971d4f
tss <tss@debian-tss24>
2016-11-17 12:49:33 +0100

    Esercizi multipli durante la lezione 3 sugli Array + continuazione esercizio da (e per) casa [Array_FullExercise]

--- src/tplez03/Esercizi/Array_FullExercise_Metodi.java
+++ src/tplez03/Esercizi/Array_FullExercise_Metodi.java
@@ -11,7 +11,7 @@ import javax.swing.JOptionPane;
  *
  * @author Emilius
  */
-public class Array_FullExercise {
+public class Array_FullExercise_Metodi {
 
     /**
      * @param args the command line arguments
@@ -25,12 +25,12 @@ public class Array_FullExercise {
         double media = 0;
         String report = "\n";               // dichiaro una variabile che userò per l'output dei dati inseriti
         
-        // creo i miei contenitori dei dati (Array)
+        // creo i miei contenitori dei dati (Array nomi e voto)
         String nomi[] = new String[max_nomi];      // il mio Array nomi prenderà la grandezza assegnata alla variabile max_nomi
         int voto[] = new int[max_nomi];
         
         
-        // chiedo l'input all'utente
+        // chiedo l'input all'utente (caricamento dati)
         String input = JOptionPane.showInputDialog("Quanti presenti ci sono?");  // chiedo l'input all'utente che mi indichi quanto deve essere grande nomi
         max_nomi = Integer.parseInt(input);         // assegno alla mia variabile "max_nomi" la grandezza indica dall'input utente
         
@@ -47,11 +47,16 @@ public class Array_FullExercise {
                                                                         // che viene incrementato ad ogni giro del mio programma
         }
         
+        /*
+        
+        
         // inizio le varie elaborazioni necessarie (voto max, voto min, media, report)
         for (int i = 0; i < nomi.length; i++) {
-            // ora mi creo un output di stampa che mi restituisca tutti i dati inseriti            
+             //ora mi creo un output di stampa che mi restituisca tutti i dati inseriti            
             report += (i + 1) + ") " + nomi[i] + " - " + voto[i] + "\n";
         }
+        
+        
             // faccio il controllo all'interno del mio array
             int max = 0;
             String nome = nomi[0];
@@ -78,12 +83,85 @@ public class Array_FullExercise {
                 gen_media = gen_media + voto[i];
                 media = gen_media / max_nomi;
             }
+            */
+        
+            //utilizzo un'altra procedura per fare le mie elaborazioni;
+            //utilizzo un "metodo" per ottenere il valore massimo contenuto in un array di numeri
+            int max = getMax(voto);  //a max assegno il valore che mi ritorna il "metodo" getMax che avrà come parametro il mio array di numeri
+            int min = getMin(voto);
+            media = getMedia(voto);
+            
+            String nomeMax;
+            
+            int indice = getIndexMax(voto);
+            
+            nomeMax = nomi[indice];
+
             
             // stampo il report
             JOptionPane.showMessageDialog(null, report);                //visualizzo l'output richiesto dei dati inseriti
             JOptionPane.showMessageDialog(null, "Il voto più alto è di: " + " " + nome + " " + max);  // output del voto max
-            JOptionPane.showMessageDialog(null, "Il voto più basso è di: " + " " + nome1 + " " +  min); // output del voto min
+            JOptionPane.showMessageDialog(null, "Il voto più basso è di: " + " " + " " +  min); // output del voto min
             JOptionPane.showMessageDialog(null, "La media dei voti è: " + " " +  media);        // output della media
             } 
-    }
+    
+    //creo un metodo "static" (è obbligatorio) 
+    //creo metodo obbligatorio static caso particolare...
+    //poi che tipo di dati deve ritornare ad esempio un numero intero .... "int" - frase... String
+    //poi nome dell'azione da fare su che cosa minuscolo verbo maiuscolo oggetto dami il massimo "getMax"
+    //obbligatorio le parentesi "()" con gli n parametri, anche 0 parametri da passare
+    // ad esempio un array da verificare dichiaro parametro di tipo corretto (int[] valori)
+    //oppure una frase (String frase) - 
+    //oppure ppiu' parametri (int x, int y, int z)
+    //poi le graffe per racchiudere il codice da eseguire SUBITO tutte e due
+    // scrivere subito il comando di return alla fine del codice del tipo dichiarato all'inizio del metodo
+    // ad esempio se ritorna un int       return  risultato;
+    // dichiarare e inizializzare all'inizio del codice la varibile usato come return
+    static int getIndexMax(int [] valori) {
+        int risultato = 0;
+        int indice = 0;
+        for (int i = 0; i < valori.length; i++) {
+                // guardo un numero per volta e guardo se è il maggiore
+                if (valori[i] > risultato) {
+                    risultato = valori[i];
+                    indice = i;  //memorizzo la posizione del dato all'interno dell'array
+                     }
+            }
+        return indice;    
+    } 
+    
+    static int getMax(int [] valori) {
+        int risultato = 0;
+        for (int i = 0; i < valori.length; i++) {
+                // guardo un numero per volta e guardo se è il maggiore
+                if (valori[i] > risultato) {
+                    risultato = valori[i];
+                     }
+            }
+        return risultato;    
+    } 
+    
+    static int getMin(int [] valori) {        
+        int risultato = valori[0];
+        for (int i = 0; i < valori.length; i++) {
+                // guardo un numero per volta e guardo se è il minore
+                if (valori[i] < risultato) {
+                    risultato = valori[i];
+                    }
+            } 
+    return risultato;    
+    } 
+    
+    static int getMedia(int[] valori) {
+        int risultato = 0;
+        int somma = 0;
+        
+        for (int i = 0; i < valori.length; i++) {
+                somma = somma + valori[i];
+                risultato = somma + valori[i];
+               }
+        return risultato;    
+    } 
+            
+}
 
